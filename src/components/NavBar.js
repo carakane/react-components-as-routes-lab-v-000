5e8a67d562e265fe0256f7e5b9ceb092dd655c32
Cara Kane <cara.kane@gmail.com>
2017-10-03 15:57:53 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,40 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink 
+        to="/"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Home Page</NavLink>
+      <NavLink 
+        to="/movies"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Movies</NavLink>
+  		
+  		<NavLink 
+        to="/directors"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Directors</NavLink>
+
+      <NavLink 
+        to="/actors"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Actors</NavLink>
     </div>
   );
 };
