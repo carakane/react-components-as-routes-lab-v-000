281714c447ce18b84f8dcfee12c3286453c66e93
Cara Kane <cara.kane@gmail.com>
2017-10-03 16:26:09 +0000

    automatically backed up by learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,9 +16,9 @@ const App = (props) => {
     <div>
       <NavBar />
       <Route exact path="/" component={Home} />
-      <Route exact path="/movies" component={Movies} movies={this.state.movies}/>
-      <Route exact path="/directors" component={Directors} directors={this.state.directors}/>
-      <Route exact path="/actors" component={Actors} actors={this.state.actors}/>
+      <Route exact path="/movies" component={Movies} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/actors" component={Actors} />
     </div>
     </Router>
   );
