68e6a923c8bee71fa2c37159898483c00ad40bb4
Cara Kane <cara.kane@gmail.com>
2017-10-03 16:20:41 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App = () => {
   <div>
 		<NavBar />
     <Route exact path="/" component={Home} />
-    <Route exact path="/movies" component={Movies} movies={this.props.movies}/>
+    <Route exact this.props.path="/movies" component={Movies} movies={this.props.movies}/>
 		<Route exact path="/directors" component={Directors} directors={this.props.directors}/>
     <Route exact path="/actors" component={Actors} actors={this.props.actors}/>
   </div>
