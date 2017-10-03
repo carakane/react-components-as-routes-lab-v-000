f035927cb27e5f01b4dd09181348d273f005dafc
Cara Kane <cara.kane@gmail.com>
2017-10-03 16:25:26 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ import { render } from 'react-dom';
 import App from './containers/App';
 import {BrowserRouter as Router, Route, Navlink} from 'react-router-dom';
 
-const App = () => {
-  return (
-<Router>
-  <div>
-		<NavBar />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/movies" component={Movies} movies={this.props.movies}/>
-		<Route exact path="/directors" component={Directors} directors={this.props.directors}/>
-    <Route exact path="/actors" component={Actors} actors={this.props.actors}/>
-  </div>
-</Router>
-  );
-};
-
-
 render (
   <App />,
   document.getElementById('root')
