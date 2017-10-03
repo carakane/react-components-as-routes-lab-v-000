9868cd5048b779b284aedcd6c8fa76d9e0406728
Cara Kane <cara.kane@gmail.com>
2017-10-03 16:26:46 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,9 +6,8 @@ const movieList = movies.map(movie => <div>{movie.title}, {movie.time} {movie.ge
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-        <h1>Movies Page</h1>
-        {movieList}
+      <h1>Movies Page</h1>
+      {movieList}
     </div>
   );
 };
