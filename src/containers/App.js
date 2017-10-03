aca76b4c9dd7242fa74fa5a00aa974163707ca3a
Cara Kane <cara.kane@gmail.com>
2017-10-03 16:25:19 +0000

    automatically backed up by learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/movies" component={Movies} movies={this.props.movies}/>
+      <Route exact path="/directors" component={Directors} directors={this.props.directors}/>
+      <Route exact path="/actors" component={Actors} actors={this.props.actors}/>
+    </div>
     </Router>
   );
 };
